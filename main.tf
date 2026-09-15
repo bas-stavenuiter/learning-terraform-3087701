e78e1bb1e883d20e7488bf140583a20c7f10dc72
Bas Stavenuiter <82209984+bas-stavenuiter@users.noreply.github.com>
2026-09-15 16:59:12 +0200

    Remove redundant name from bkog_sg module
    Removed redundant name attribute from security group module.

--- main.tf
+++ main.tf
@@ -32,9 +32,7 @@ module "bkog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
   name = "blog_new"
-
   vpc_id = data.aws_vpc.default.id
-  name    = "blog"
 
   ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
